542da0e4965c20e99005581f05cf88a72ca7c265
Chase <c@chse.dev>
2022-04-20 14:40:09 -0500

    v1.0.0

---
+++ README.md
@@ -0,0 +1,14 @@
+<div align="center">
+<h1>ESLint Config<br>
+<a href="https://github.com/ChxseH/eslint-config/blob/master/LICENSE.md"><img alt="GitHub license" src="https://img.shields.io/github/license/ChxseH/eslint-config"></a>
+<a href="https://github.com/ChxseH/eslint-config/stargazers"><img alt="GitHub stars" src="https://img.shields.io/github/stars/ChxseH/eslint-config"></a>
+<a href="https://github.com/ChxseH/eslint-config/issues"><img alt="GitHub issues" src="https://img.shields.io/github/issues/ChxseH/eslint-config"></a>
+<a href="https://github.com/ChxseH/eslint-config/network"><img alt="GitHub forks" src="https://img.shields.io/github/forks/ChxseH/eslint-config"></a>
+<a href="https://github.com/ChxseH/eslint-config/actions/workflows/linter.yml"><img alt="GitHub Actions Status" src="https://github.com/ChxseH/eslint-config/actions/workflows/linter.yml/badge.svg"></a>
+</h1></div>
+
+## Installation  
+
+```bash
+
+```
---
+++ index.js
@@ -0,0 +1,112 @@
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = {
+
+    extends: [
+        `eslint:recommended`,
+        `plugin:unicorn/recommended`,
+        `plugin:jsdoc/recommended`,
+        `airbnb-base`
+    ],
+
+    plugins: [
+        `unicorn`,
+        `jsdoc`
+    ],
+
+    rules: {
+        "no-unused-vars": `error`,
+        "no-redeclare": `off`,
+        quotes: [
+            `error`,
+            `backtick`
+        ],
+        camelcase: [
+            `error`,
+            {
+                properties: `always`
+            }
+        ],
+        indent: [
+            `error`,
+            4,
+            {
+                SwitchCase: 1
+            }
+        ],
+        "max-len": [
+            `error`,
+            {
+                code: 160,
+                ignoreUrls: true,
+                ignoreComments: true,
+                ignoreStrings: true,
+                ignoreTemplateLiterals: true
+            }
+        ],
+        "no-console": `off`,
+        "no-restricted-syntax": [
+            `error`,
+            {
+                selector: `CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]`,
+                message: `Unexpected property on console object was called`
+            }
+        ],
+        "brace-style": [
+            `error`,
+            `allman`,
+            {
+                allowSingleLine: true
+            }
+        ],
+        "template-curly-spacing": [
+            `error`,
+            `always`
+        ],
+        "no-use-before-define": `off`,
+        "comma-dangle": `off`,
+        "consistent-return": `off`,
+        curly: [
+            `error`,
+            `multi-or-nest`
+        ],
+        "nonblock-statement-body-position": `off`,
+        "vars-on-top": `off`,
+        "no-plusplus": [
+            `error`,
+            {
+                allowForLoopAfterthoughts: true
+            }
+        ],
+        "no-await-in-loop": `off`,
+        "no-shadow": `off`,
+        "no-var": `off`,
+        "no-loop-func": `off`,
+        "func-names": `off`,
+        "prefer-destructuring": `off`,
+        "block-scoped-var": `off`,
+        "no-param-reassign": `off`,
+        "no-multiple-empty-lines": [
+            `error`,
+            {
+                max: 1,
+                maxBOF: 0,
+                maxEOF: 1
+            }
+        ],
+        "padded-blocks": [
+            `error`,
+            `never`
+        ],
+        "default-case": `off`,
+        "import/extensions": [
+            `error`,
+            `ignorePackages`,
+            {
+                js: `always`
+            }
+        ],
+        "import/no-unresolved": `off`,
+        "unicorn/no-process-exit": `off`,
+        "unicorn/filename-case": `off`
+    },
+};
